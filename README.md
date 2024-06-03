149de9723832f7b1e7a02723df17fbad712dcb6c
jhuffm8 <jordan.huffman85@gmail>
2024-06-03 09:24:04 -0600

    created server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Capstone-BudgetingApp
 A budgeting app that tracks income and expenses
+able to enter transactions 
+keep up to date with overall spending and earnings
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+
+
+const app = express()
+const PORT = process.env.PORT
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
